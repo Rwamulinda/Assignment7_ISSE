f631bc3245b666f52284f77d5f93c05da59f6b01
uwasepauline7 <puwase@andrew.cmu.edu>
2024-10-16 20:09:21 +0200

    adding a header

--- cat.c
+++ cat.c
@@ -3,56 +3,90 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
 
-#define CAT_HERDER "kitty"
+#define KITTYPATH "kitty"
 
-void run_kitty(int runlevel) {
-    // Prepare arguments for execvp
+void run_kitty(const char *arg, char *envp[]) {
     char *args[3];
-    args[0] = CAT_HERDER;
-    args[1] = "-n"; // Set the runlevel argument
+    args[0] = KITTYPATH;
+    args[1] = (char *)arg;
     args[2] = NULL;
 
-    // Fork a child process to run kitty
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(1);
-    }
-
-    if (pid == 0) { // Child process
-        // Execute the kitty program
-        execvp(args[0], args);
-        perror("execvp"); // If execvp fails
-        exit(1);
-    } else { // Parent process
-        // Wait for the child to finish
-        int status;
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) {
-            int exit_status = WEXITSTATUS(status);
-            printf("kitty exited with status: %d\n", exit_status);
-        } else {
-            printf("kitty did not exit normally.\n");
-        }
-    }
+    execve(KITTYPATH, args, envp);
+    perror("execve"); // Only reached if execve fails
+    exit(1);
 }
 
 int main(int argc, char *argv[], char *envp[]) {
-    // Set environment variables before running kitty
-    setenv("CATFOOD", "yummy", 1);
-    setenv("PATH", getenv("PATH"), 1); // Ensure PATH is set
-    setenv("HOME", getenv("HOME"), 1); // Ensure HOME is set
-    unsetenv("KITTYLITTER"); // Ensure KITTYLITTER is not set
-
-    // Check for the runlevel from command-line arguments (default to 2 for this example)
-    int runlevel = 2; // You can modify this based on your needs
-    if (argc > 1) {
-        runlevel = atoi(argv[1]);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <inputfile> <outputfile>\n", argv[0]);
+        return 1;
     }
 
-    // Run the kitty program
-    run_kitty(runlevel);
+    const char *inputfile = argv[1];
+    const char *outputfile = argv[2];
 
-    return 0;
+    if (strcmp(inputfile, outputfile) == 0) {
+        fprintf(stderr, "Error: inputfile and outputfile must be different.\n");
+        return 1;
+    }
+
+    int pipe1[2], pipe2[2];
+    pipe(pipe1);
+    pipe(pipe2);
+
+    // Fork first child
+    if (fork() == 0) {
+        // Redirect stdin to inputfile
+        int in_fd = open(inputfile, O_RDONLY);
+        dup2(in_fd, STDIN_FILENO);
+        close(in_fd);
+
+        // Run kitty -2
+        run_kitty("-2", envp);
+    }
+
+    // Fork second child
+    if (fork() == 0) {
+        // Redirect stdin to the first pipe and stdout to the second pipe
+        dup2(pipe1[0], STDIN_FILENO);
+        dup2(pipe2[1], STDOUT_FILENO);
+        close(pipe1[1]);
+        close(pipe2[0]);
+
+        // Run kitty -3
+        run_kitty("-3", envp);
+    }
+
+    // Fork third child
+    if (fork() == 0) {
+        // Redirect stdin to the second pipe and stdout to outputfile
+        dup2(pipe2[0], STDIN_FILENO);
+        int out_fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        dup2(out_fd, STDOUT_FILENO);
+        close(pipe2[1]);
+        close(out_fd);
+
+        // Run kitty -4
+        run_kitty("-4", envp);
+    }
+
+    // Close all pipe ends in the parent
+    close(pipe1[0]);
+    close(pipe1[1]);
+    close(pipe2[0]);
+    close(pipe2[1]);
+
+    // Wait for all children
+    int status;
+    for (int i = 0; i < 3; i++) {
+        wait(&status);
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+            return 1; // If any child fails, return 1
+        }
+    }
+
+    return 0; // Success
 }
