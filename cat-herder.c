e3c6ba2369b4bde62a1cbef11e83ddc5a60a0508
uwasepauline7 <puwase@andrew.cmu.edu>
2024-10-19 15:18:55 +0200

    adding a file

--- cat-herder.c
+++ cat-herder.c
@@ -23,6 +23,9 @@ int main() {
             // Set the PATH environment variable to include /home/puwase
             setenv("PATH", "/home/puwase:/usr/bin:/bin", 1);
 
+            // Set the CATFOOD environment variable
+            setenv("CATFOOD", "tuna", 1);  // You can change "tuna" to any appropriate value
+
             // Use execl to run /var/local/isse-07/kitty with an argument indicating the child number
             char arg[3];
             snprintf(arg, sizeof(arg), "-%d", i);  // Format "-0", "-1", "-2"
