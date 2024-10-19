cc6ebf0f9777ae01ec35a0aad963f8472be47190
uwasepauline7 <puwase@andrew.cmu.edu>
2024-10-19 16:10:19 +0200

    adding a file

--- cat-herder.c
+++ cat-herder.c
@@ -28,9 +28,8 @@ int main() {
         }
 
         if (pid[i] == 0) {  // Child process
-            // Set the PATH environment variable to include /home/puwase
-            // Set the PATH environment variable to include /home/puwase and other directories
-            setenv("PATH", "/home/puwase:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin", 1);
+            // Set the PATH environment variable to include the expected directories
+            setenv("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/var/local/scottycheck/isse-07", 1);
 
             // Set the CATFOOD environment variable
             setenv("CATFOOD", "yummy", 1);  // You can change "yummy" to any appropriate value
