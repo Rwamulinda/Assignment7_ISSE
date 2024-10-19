0c928c6bfef5b2f135c22b91be0c63c1b53ec311
uwasepauline7 <puwase@andrew.cmu.edu>
2024-10-19 17:02:32 +0200

    adding a file

--- cat-herder.c
+++ cat-herder.c
@@ -15,8 +15,8 @@ int main(int argc, char *argv[]) {
 
     const char *input_file = argv[1];
     const char *output_file = argv[2];
-    pid_t pid[3];
-    int pipefd[2][2];
+    pid_t pid[3];         // Store PIDs of child processes
+    int pipefd[2][2];      // Two pipes for three processes
 
     // Create pipes
     for (int i = 0; i < 2; i++) {
@@ -26,12 +26,14 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Open output file
     int out_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (out_fd == -1) {
         perror("open output file");
         exit(EXIT_FAILURE);
     }
 
+    // Fork child processes
     for (int i = 0; i < 3; i++) {
         pid[i] = fork();
 
@@ -41,55 +43,64 @@ int main(int argc, char *argv[]) {
         }
 
         if (pid[i] == 0) {  // Child process
+            // Set environment variables
             setenv("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/var/local/scottycheck/isse-07", 1);
             setenv("CATFOOD", "yummy", 1);
 
-            if (i > 0) {
-                dup2(pipefd[i - 1][0], STDIN_FILENO);
-            } else {
+            // Set up input redirection
+            if (i == 0) {  // First child reads from input file
                 int in_fd = open(input_file, O_RDONLY);
                 if (in_fd == -1) {
                     perror("open input file");
                     exit(EXIT_FAILURE);
                 }
                 dup2(in_fd, STDIN_FILENO);
-                close(in_fd);
+                close(in_fd);  // Close after redirecting
+            } else {  // Other children read from the previous pipe
+                dup2(pipefd[i - 1][0], STDIN_FILENO);
             }
 
-            if (i < 2) {
+            // Set up output redirection
+            if (i < 2) {  // First two children write to the next pipe
                 dup2(pipefd[i][1], STDOUT_FILENO);
-            } else {
+            } else {  // Last child writes to the output file
                 dup2(out_fd, STDOUT_FILENO);
             }
 
+            // Close all unused pipe ends in the child
             for (int j = 0; j < 2; j++) {
                 close(pipefd[j][0]);
                 close(pipefd[j][1]);
             }
 
+            // Execute kitty with the appropriate argument
             char arg[3];
             snprintf(arg, sizeof(arg), "-%d", i);
-
             execl(KITTY_EXEC, "kitty", arg, NULL);
+
+            // If exec fails, print error and exit
             perror("execl");
             exit(EXIT_FAILURE);
         }
     }
 
+    // Parent closes all pipe ends
     for (int i = 0; i < 2; i++) {
         close(pipefd[i][0]);
         close(pipefd[i][1]);
     }
 
+    // Close output file descriptor in parent
     close(out_fd);
 
+    // Wait for all child processes to complete
     for (int i = 0; i < 3; i++) {
         int status;
         waitpid(pid[i], &status, 0);
 
         if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
             fprintf(stderr, "Child %d exited with status %d\n", i, WEXITSTATUS(status));
-            exit(EXIT_FAILURE);
+            exit(EXIT_FAILURE);  // Exit if any child fails
         }
     }
 
