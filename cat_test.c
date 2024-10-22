1bdaf1b84eae0eb8aae33e163b8d47798a8a52a7
uwasepauline7 <puwase@andrew.cmu.edu>
2024-10-22 15:48:12 +0200

    testing

--- cat_test.c
+++ cat_test.c
@@ -53,42 +53,52 @@ int main(int argc, char *argv[]) {
         }
 
         if (pid[i] == 0) { // Child process
-            // Set environment variables according to the child
-            if (i == 0) { // kitty -2
+            // Redirect input/output
+            if (i == 0) { // First child (kitty -2)
                 setenv("CATFOOD", "yummy", 1);
-                unsetenv("KITTYLITTER"); // Remove KITTYLITTER if set
-                char *new_env[] = {
-                    "PATH=/usr/bin:/home/puwase", // Ensure it contains the home directory
-                    "HOME=/home/puwase",
-                    "CATFOOD=yummy",
-                    NULL
-                };
-                dup2(pipefd[0][1], STDOUT_FILENO); // Redirect output to pipe
-                execle(KITTY_EXEC, "kitty", "-2", NULL, new_env);
-            } else if (i == 1) { // kitty -3
                 unsetenv("KITTYLITTER");
+                dup2(pipefd[0][1], STDOUT_FILENO); // Redirect stdout to the first pipe
+                close(pipefd[0][0]); // Close read end of the first pipe
+                close(pipefd[1][0]); // Close unused read end of the second pipe
+                close(pipefd[1][1]); // Close unused write end of the second pipe
+
+                // Execute kitty -2
                 char *new_env[] = {
-                    "PATH=/usr/bin:/home/puwase", // Ensure it contains the home directory
-                    "HOME=/home/puwase",
-                    NULL
-                };
-                dup2(pipefd[1][0], STDIN_FILENO); // Read from the previous pipe
-                execle(KITTY_EXEC, "kitty", "-3", NULL, new_env);
-            } else if (i == 2) { // kitty -4
-                setenv("CATFOOD", "yummy", 1);
-                char *new_env[] = {
-                    "PATH=/usr/bin:/home/puwase", // Ensure it contains the home directory
+                    "PATH=/usr/bin:/home/puwase",
                     "HOME=/home/puwase",
                     "CATFOOD=yummy",
                     NULL
                 };
-                dup2(out_fd, STDOUT_FILENO); // Write to the output file
+                execle(KITTY_EXEC, "kitty", "-2", NULL, new_env);
+            } else if (i == 1) { // Second child (kitty -3)
+                dup2(pipefd[0][0], STDIN_FILENO); // Read from first pipe
+                dup2(pipefd[1][1], STDOUT_FILENO); // Write to second pipe
+                close(pipefd[0][1]); // Close write end of the first pipe
+                close(pipefd[1][0]); // Close unused read end of the second pipe
+
+                // Execute kitty -3
+                char *new_env[] = {
+                    "PATH=/usr/bin:/home/puwase",
+                    "HOME=/home/puwase",
+                    NULL
+                };
+                execle(KITTY_EXEC, "kitty", "-3", NULL, new_env);
+            } else if (i == 2) { // Third child (kitty -4)
+                dup2(pipefd[1][0], STDIN_FILENO); // Read from second pipe
+                dup2(out_fd, STDOUT_FILENO); // Write to output file
+                close(pipefd[0][0]); // Close read end of the first pipe
+                close(pipefd[0][1]); // Close write end of the first pipe
+                close(pipefd[1][1]); // Close write end of the second pipe
+
+                // Execute kitty -4
+                char *new_env[] = {
+                    "PATH=/usr/bin:/home/puwase",
+                    "HOME=/home/puwase",
+                    "CATFOOD=yummy",
+                    NULL
+                };
                 execle(KITTY_EXEC, "kitty", "-4", NULL, new_env);
             }
-
-            // Close all pipes in the child process
-            close_all_pipes(pipefd);
-            close(out_fd); // Ensure output file is closed
 
             // If exec fails, exit
             perror("execle");
@@ -97,7 +107,8 @@ int main(int argc, char *argv[]) {
     }
 
     // Parent process: Close all pipe write ends
-    close_all_pipes(pipefd);
+    close(pipefd[0][1]);
+    close(pipefd[1][1]);
     close(out_fd); // Close output file in parent
 
     // Wait for all child processes to complete
